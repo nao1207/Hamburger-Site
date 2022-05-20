ea78a0aeca903c60f210315ebff15ead8231241e
neo1207 <chagucaixu@gmail.com>
2022-05-21 06:44:15 +0900

    grit導入不具合有り

--- script.js
+++ script.js
@@ -1,5 +1,30 @@
 $(function(){
     $('.c-sidebar__button').click(function(){
-        $('.l-sidebar').toggleClass('open');
+        $('.l-sidebar').addClass('open');
+        $('.l-header').addClass('open');
+        $('.wrapper').addClass('open');
+        $('.l-footer').addClass('open');
+        $('.c-sidebar__button--close').addClass('open');
     });
+    $('.c-sidebar__button--close').click(function(){
+        $('.l-sidebar.open').removeClass('open');
+        $('.l-header.open').removeClass('open');
+        $('.wrapper.open').removeClass('open');
+        $('.l-footer.open').removeClass('open');
+        $('.c-sidebar__button--close.open').removeClass('open');
+        
+    })
 });
+
+/*
+$(function(){
+    $('.c-sidebar__button').click(function(){
+        $('.l-sidebar').addClass('open');
+        $('.c-section .p-main-visual .l-footer').toggleClass('open');
+    });
+    $('.c-sidebar__button--close').click(function(){
+        $('.l-sidebar.open').removeClass('open');
+        
+    })
+});
+*/
