cfa4da4330b1a8145e2b2d198238640cede9bed7
neo1207 <chagucaixu@gmail.com>
2022-05-31 11:29:45 +0900

    スライド修正ほぼ完了

--- script.js
+++ script.js
@@ -1,3 +1,64 @@
+
+$(function(){
+    $('.c-button--sidebar').click(function(){
+        $('.l-sidebar').addClass('open');
+        $('.main-watermark_bg').addClass('open');
+        $('body').addClass('open');
+        //$('.c-sidebar__button--close').addClass('open');
+    })
+    $('.c-sidebar__button--close').click(function(){
+        $('.l-sidebar.open').removeClass('open');
+        $('.main-watermark_bg.open').removeClass('open');
+        $('body.open').removeClass('open');
+        //$('.c-sidebar__button--close.open').removeClass('open');
+    })
+});
+/*
+$(function(){
+    $('.c-button--sidebar').click(function(){
+        $('.l-sidebar').addClass('open');
+        $('.main-watermark_bg').addClass('open');
+        $('body').addClass('open');
+        $('.c-sidebar__button--close').addClass('open');
+
+    });
+    $('.c-sidebar__button--close').click(function(){
+        $('.l-sidebar.open').removeClass('open');
+        $('.main-watermark_bg').removeClass('open');
+        $('body.open').removeClass('open');
+        $('.c-sidebar__button--close.open').removeClass('open');
+    })
+});
+*/
+/*
+$('.c-button--sidebar').click(function(){
+    $('l-sidebar').addClass('open');
+    $('.l-sidebar.open').slideToggle();
+})
+*/
+/*
+$(function(){
+    $('.c-button--sidebar').click(function(){
+    //if($('.l-sidebar__section').hasClass('close')){
+        //$('.l-sidebar__section').removeClass('close');
+        $('.l-sidebar').addClass('open');
+        $('.c-sidebar__button--close').addClass('open');
+        $('.l-sidebar__section').animate({'marginRight':'1px'},500);
+        $('.main-watermark_bg').addClass('close');
+        $('body').addClass('open');
+    //}
+    });
+    $('.c-sidebar__button--close').click(function(){
+        //$('.l-sidebar__section').addClass('close');
+        $('.l-sidebar.open').removeClass('open');
+        $('.c-sidebar__button--close.open').removeClass('open');
+        $('l-sidebar__section').animate({'marginRight':'0'},500);
+        $('.main-watermark_bg').removeClass('close');
+        $('body.open').removeClass('open');
+    })
+});
+*/
+/*
 $(function(){
     $('.c-button--sidebar').click(function(){
         $('.l-sidebar').addClass('open');
@@ -12,3 +73,4 @@ $(function(){
         $('.c-sidebar__button--close.open').removeClass('open');
     })
 });
+*/
