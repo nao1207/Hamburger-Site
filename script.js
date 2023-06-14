3f555a424cbb99694e36f09605fb9b9ec1b2a53d
neo1207 <chagucaixu@gmail.com>
2023-06-14 21:34:00 +0900

    jQuery苦戦中

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
-$(function(){
-//jQuery(function($){
+//$(function(){
+    //Menuボタンを押した時の処理
+    jQuery(function($){
     $('.c-button--sidebar').click(function(){
         $('.l-sidebar').addClass('open');
         $('.main-watermark_bg').addClass('open');
@@ -19,26 +20,3 @@ $(window).on('load resize',function(){
         $('body.open').removeClass('open');
     }
 });
-
-/*
-$(function(){
-    $('.c-button--sidebar').click(function(){
-    if($('.l-sidebar__section').hasClass('close')){
-        //$('.l-sidebar__section').removeClass('close');
-        $('.l-sidebar').addClass('open');
-        $('.c-sidebar__button--close').addClass('open');
-        $('.l-sidebar__section').animate({'marginRight':'1px'},500);
-        $('.main-watermark_bg').addClass('close');
-        $('body').addClass('open');
-    }
-    });
-    $('.c-sidebar__button--close').click(function(){
-        $('.l-sidebar__section').addClass('close');
-        $('.l-sidebar.open').removeClass('open');
-        $('.c-sidebar__button--close.open').removeClass('open');
-        $('l-sidebar__section').animate({'marginRight':'0'},500);
-        $('.main-watermark_bg').removeClass('close');
-        $('body.open').removeClass('open');
-    })
-});
-*/
